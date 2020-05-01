49199cf43ddfcae4d8babe9853ff80ed3428e18a
Sarad <saradcd77@mgail.com>
2020-05-01 15:39:11 -0700

    magic_methods

--- magic_methods.py
+++ magic_methods.py
@@ -2,10 +2,14 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+     	self.dict = {"a":1,"b":2}
 
     # Return the attributes of the class using inbuilt string method
-    def __str__(self):
-        return f"({self.x},{self.y})"
+    # def __str__(self):
+    #     return f"({self.x},{self.y})"
+
+    def __getitem__(self,key):
+    	return self.dict[key]
 
     # Compares two object using inbuilt method greater than
     def __gt__(self, other):
@@ -21,3 +25,6 @@ other = Point(3, 4)
 print(point < other) #Printes True
 print(other > point ) #Prints True
 print(point) #Prints value of point object (1,2)
+
+print(point.__getitem__("a")) # returns value of a i.e 1
+print(point.__getitem__("d")) # Key error
