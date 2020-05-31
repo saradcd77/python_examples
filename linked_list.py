6ccac7c98d51a3cb84ae55aff8172ef840499f6a
Sarad <saradcd77@gmail.com>
2020-05-31 13:45:29 -0700

    Linked List in python

---
+++ linked_list.py
@@ -0,0 +1,17 @@
+""" LinkedList in Python """
+
+class Node:
+	def __init__(self, val):
+		self.next = next
+		self.val = val
+
+	def __str__(self):
+		return str(self.val)
+
+
+
+
+a = Node(1)
+
+print(a.next.val)
+print(a)
